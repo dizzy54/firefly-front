63867a2eb714431b74c0b95b67945efa2af91683
Akshat <akshat54@gmail.com>
2015-12-08 14:20:03 +0530

    BLE enabling added

--- Firefly/app/src/main/java/com/dizzy1354/firefly/MainActivity.java
+++ Firefly/app/src/main/java/com/dizzy1354/firefly/MainActivity.java
@@ -1,5 +1,11 @@
 package com.dizzy1354.firefly;
 
+import android.annotation.TargetApi;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothManager;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,8 +14,11 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    BluetoothAdapter mBluetoothAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +35,20 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        // check if ble available. Return True or False
+        Boolean ble_available = checkBleAvailable();
+
+        //
+        if (ble_available) {
+            BluetoothManager bluetoothManager = initializeBle((Context) this);
+            mBluetoothAdapter = bluetoothManager.getAdapter();
+            enableBle();
+
+        }
+        else {
+            // write code to cover any views in app for unavailability of BLE
+        }
     }
 
     @Override
@@ -49,4 +72,37 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    public Boolean checkBleAvailable() {
+        // Use this check to determine whether BLE is supported on the device. Then
+        // you can selectively disable BLE-related features.
+        Boolean return_val = true;
+        if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)) {
+            Toast.makeText(this, R.string.ble_not_supported, Toast.LENGTH_SHORT).show();
+            return_val = false;
+            //finish();
+        }
+        return return_val;
+    }
+
+    @TargetApi(18)
+    public BluetoothManager initializeBle(Context context) {
+        // Initializes Bluetooth adapter. Returns manager
+        final BluetoothManager bluetoothManager =
+                (BluetoothManager) getSystemService(context.BLUETOOTH_SERVICE);
+        return bluetoothManager;
+    }
+
+    @TargetApi(18)
+    public void enableBle(){
+        // Ensures Bluetooth is available on the device and it is enabled. If not,
+        // displays a dialog requesting user permission to enable Bluetooth.
+        Boolean enabled = false;
+        int REQUEST_ENABLE_BT = 1;
+        if (mBluetoothAdapter == null || !mBluetoothAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+        }
+    }
+
 }
